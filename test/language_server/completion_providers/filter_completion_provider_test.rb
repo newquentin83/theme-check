6b5a28d481d6823ad9bea3de368a147d303358ac
Julien Poitrin <julien.poitrin@shopify.com>
2022-11-25 09:55:06 +0100

    Add unit tests

--- test/language_server/completion_providers/filter_completion_provider_test.rb
+++ test/language_server/completion_providers/filter_completion_provider_test.rb
@@ -26,6 +26,19 @@ module ThemeCheck
         assert_can_complete_with(@provider, "{{ 'foo.js' | asset_url | image", "image_url")
       end
 
+      def test_suggest_filters_compatible_with_the_array_type
+        assert_can_complete_with(@provider, "{{ current_tags | ", "compact")
+        refute_can_complete_with(@provider, "{{ current_tags | ", "url_decode")
+      end
+
+      def test_suggest_filters_compatible_with_the_string_type
+        assert_can_complete_with(@provider, "{{ image_url | ", "url_decode")
+        refute_can_complete_with(@provider, "{{ image_url | ", "compact")
+        # TODO: assert_can_complete_with(@provider, "{{ 'test%40test.com' | ", "url_decode")
+      end
+
+      # TODO: Add test for "assign c = product.collections | "
+
       def test_does_not_complete_deprecated_filters
         refute_can_complete_with(@provider, "{{ 'foo.js' | hex_to", "hex_to_rgba")
       end
